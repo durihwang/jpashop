1d2de448372c233882d10f6413a41e5f3ce61252
황두리 <hwangduli0515@gmail.com>
2021-10-31 22:06:15 +0900

    회원가입, 상품등록, 주문 개발

--- src/main/java/com/jpabook/jpashop/domain/Address.java
+++ src/main/java/com/jpabook/jpashop/domain/Address.java
@@ -1,11 +1,15 @@
 package com.jpabook.jpashop.domain;
 
+import lombok.AccessLevel;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.RequiredArgsConstructor;
 
 import javax.persistence.Embeddable;
 
 @Embeddable
 @Getter
+@NoArgsConstructor
 public class Address {
 
     private String city;
@@ -13,5 +17,11 @@ public class Address {
     private String street;
 
     private String zipcode;
+
+    public Address(String city, String street, String zipcode) {
+        this.city = city;
+        this.street = street;
+        this.zipcode = zipcode;
+    }
 
 }
